65d228b1de606cf820582ad995b8f8b02e615344
Sarmat <Spirator@Yandex.ru>
2023-08-25 18:07:24 +0700

    Нашел ещё 2-а варианта решения задачи

--- Task_1.py
+++ Task_1.py
@@ -23,4 +23,63 @@ else:
     else:
         print(f"Минимальное кол-во монет, которые нужно перевернуть: {b}")
         
+        
+        
 
+# Вариант 2.
+# n = int(input("Введите общее кол-во монет: "))
+# tails = int(input("Введите кол-во монет, которые лежат вверх решкой: "))
+# emblem = int(input("Введите кол-во монет, которые лежат вверх гербом: "))
+
+# if tails > n or tails < 0 or emblem > n or emblem < 0 or tails == emblem:
+#     print("Введено неверное кол-во монет повторите попытку")
+# else:
+#     min_1 = float("inf")
+#     i = 0
+    
+#     if tails > emblem:
+#         while i <= n:
+#             if n - i == tails and i == emblem:
+#                 min_1 = i
+#                 break
+#             i += 1
+#     else:
+#         while i <= n:
+#             if n - i == emblem and i == tails:
+#                 min_1 = i
+#                 break
+#             i += 1
+        
+#     if min_1 == float("inf"):
+#         print("Невозможно найти решение")
+#     else:
+#         print(f"Минимальное кол-во монет, которые нужно перевернуть: {min_1}")
+
+
+            
+    
+# Вариант 3.
+# n = int(input("Введите общее кол-во монет: "))
+# tails = int(input("Введите кол-во монет, которые лежат вверх решкой: "))
+# emblem = int(input("Введите кол-во монет, которые лежат вверх гербом: "))
+
+# if tails > n or tails < 0 or emblem > n or emblem < 0 or tails == emblem:
+#     print("Введено неверное кол-во монет повторите попытку")
+# else:
+#     min_1 = float("inf")
+    
+#     if tails > emblem:
+#         for i in range(n + 1):
+#             if n - i == tails and i == emblem:
+#                 min_1 = i
+#                 break
+#     else:
+#         for i in range(n + 1):
+#             if n - i == emblem and i == tails:
+#                 min_1 = i
+#                 break
+        
+#     if min_1 == float("inf"):
+#         print("Невозможно найти решение")
+#     else:
+#         print(f"Минимальное кол-во монет, которые нужно перевернуть: {min_1}")
